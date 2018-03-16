a6404d2a3882df3c32a3873b4853ec94d8109d92
beta3000 <atlas.aresco@gmail.com>
2018-03-16 13:04:17 -0500

    Adding an Image Preview.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -17,13 +17,13 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
     this.subscription = this.slService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
       );
+    this.ingredients = this.slService.getIngredients();
   }
 
   onEditItem(index: number) {
