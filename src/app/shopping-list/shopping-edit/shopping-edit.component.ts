aabfcf5fe9737b538a0fb618d073cbc70cbc65b9
beta3000 <atlas.aresco@gmail.com>
2018-03-16 09:11:37 -0500

    Resetting the Form.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -43,6 +43,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
   }
 
   ngOnDestroy(): void {
